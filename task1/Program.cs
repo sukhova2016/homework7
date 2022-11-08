830d60ef2fce3541b0af5a35b7c54e8e19709bb0
Polina Drokina <sukhova2016@inbox.ru>
2022-11-08 11:28:05 +0400

    Добавлено решение первой задачи

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите количество строк массива");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[m, n];
+
+Random random = new Random();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = random.NextDouble()*10;
+        Console.Write("{0,6:F1}", array[i, j]);
+    }
+    Console.WriteLine();
+}
