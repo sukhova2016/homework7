b1bdd43aec4b02ebe9dea3f952e0cbd0d60d74dd
Polina Drokina <sukhova2016@inbox.ru>
2022-11-08 14:22:20 +0400

    Добавлено решение третьей задачи

---
+++ task3/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите количество строк массива");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+Random random = new Random();
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(1, 20);
+            Console.Write("{0,4}", array[i, j]);
+        }
+        Console.WriteLine();
+    }
+
+    double sum = 0;
+    Console.Write($"Среднее арифметическое каждого столбца:  ");
+
+    for (int j = 0; j < n; j++)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            sum += array[i, j];       
+        }   
+        Console.Write("{0,6:F1}", (sum / m));
+    }
+
+}
+
+
